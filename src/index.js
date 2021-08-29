9bdabcdf2ef72841e28fd7be09d1d6781bdd6a48
prinxard <prinxard@gmail.com>
2021-08-29 22:40:53 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
 
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
